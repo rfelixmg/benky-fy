3fb893cc265b3cd638470c647b86d58e7f716af4
rfelixmg <rfelixmg@gmail.com>
2025-08-23 18:54:27 +0900

    running well

--- run.py
+++ run.py
@@ -5,7 +5,8 @@ app = create_app()
 
 
 if __name__ == "__main__":
-	app.run(debug=True, host='localhost', port=5000)
+	# app.run(debug=True, host='localhost', port=5000)
+	app.run(debug=True, host='0.0.0.0', port=8080)
 	# app.run()
 
 
