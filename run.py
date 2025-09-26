c8e2b45a49003c6080d8fbdad9678a9fb02c93c9
rfelixmg <rfelixmg@gmail.com>
2025-09-26 15:40:23 +0900

    pretty

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ except Exception as e:
 if __name__ == "__main__":
     # app.run(debug=True, host='localhost', port=5000)
     # app.run(debug=True, host='localhost', port=8080)
-    app.run(debug=True, host='localhost', port=8081)
+    app.run(debug=True, host='localhost', port=8080)
     # app.run(debug=True, host='0.0.0.0', port=8080)
     # app.run()
 
